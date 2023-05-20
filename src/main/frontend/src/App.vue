1a847639fe58d61871a8a17e0d5149551a9bcfcd
PatrycjaAGH <123194532+PatrycjaAGH@users.noreply.github.com>
2023-05-20 21:20:26 +0200

    Update App.vue

--- src/main/frontend/src/App.vue
+++ src/main/frontend/src/App.vue
@@ -4,20 +4,22 @@
 
     <div v-if="authenticatedUsername">
       <UserPanel :username="authenticatedUsername" @logout="logMeOut()"></UserPanel>
-      <MeetingsPage :username="authenticatedUsername"></MeetingsPage>
+      <MeetingsPage :username="authenticatedUsername" :meetings="meetings"></MeetingsPage>
     </div>
 
     <div v-else>
-      <button :class="signingUp ? 'button-outline' : ''" @click="signingUp = false">Logowanie</button>
-      <button :class="!signingUp ? 'button-outline' : ''" @click="signingUp = true">Rejestracja</button>
+      <button :class="signingUp ? 'button-outline' : '' "@click="signingUp = false"> Logowanie </button>
+      <button :class="!signingUp ? 'button-outline' : '' "@click="signingUp = true"> Rejestracja </button>
 
-     <div v-if="" class="alert">
-       {{message}}
-     </div>
+      <div v-if="message" class="['alert', 'alert-' + (this.isError ? 'error' : 'success')]">
+      </div>
+      <div :class="['alert', 'alert-' + (this.isError ? 'error' : 'success')]" v-if="message">
+        {{ message }}
+
+      </div>
 
       <LoginForm v-if="!signingUp" @login="(user) => logMeIn(user)"></LoginForm>
       <LoginForm v-else @login="(user) => register(user)" button-label="Załóż konto"></LoginForm>
-
     </div>
   </div>
 </template>
@@ -27,33 +29,60 @@ import "milligram";
 import LoginForm from "./LoginForm";
 import UserPanel from "./UserPanel";
 import MeetingsPage from "./meetings/MeetingsPage";
-import axios from "axios";
+import axios from "axios"
 
 export default {
   components: {LoginForm, MeetingsPage, UserPanel},
   data() {
     return {
-      message: '',
+      message: "",
       signingUp: false,
       authenticatedUsername: '',
+      meetings: [],
+      isError: false
     }
   },
+
   methods: {
     logMeIn(user) {
-      this.authenticatedUsername = user.login;
+      axios.post("/api/tokens", user)
+          .then(response => {
+            this.authenticatedUsername = user.login;
+            const token = response.data.token;
+            axios.defaults.headers.common['Authorization'] = 'Bearer ' + token;
+            axios.get('/api/meetings')
+                .then(response => {
+                  this.meetings = response.data;
+                })
+                .catch(response => {
+                  this.isError = true;
+                  this.message = ("Nie udało się pobrać listy spotkań")
+                })
+            this.isError = false;
+            this.message = ("Udało się zalogować")
+          })
+          .catch(response => {
+            this.isError = true;
+            this.message = ("Nie udało się zalogować")
+          })
     },
     logMeOut() {
       this.authenticatedUsername = '';
+      delete axios.defaults.headers.common.Authorization;
     },
+
     register(user) {
       axios.post('/api/participants', user)
           .then(response => {
-            this.message = 'Udało się założyc konto';
+            this.isError = false;
+            this.message = ("Udało się założyć konto")
+
           })
           .catch(response => {
-            this.message = 'Nie udało się założyć konto';
+            this.isError = true;
+            this.message = ("Nie udało się założyć konta")
           });
-    }
+    },
   }
 }
 </script>
@@ -65,8 +94,17 @@ export default {
 }
 
 .alert {
+  border: 1px solid black;
+  font-size: 20px;
   padding: 5px;
-  margin:30px;
-  color: black;
+}
+
+.alert-success {
+  background: darkseagreen;
+  border-color: green;
+}
+.alert-error {
+  background: palevioletred;
+  border-color: darkred;
 }
 </style>
